515ef9e832306eecf879c5c7c69c1fe7572b6dfc
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-11 15:58:47 -0600

    Add jumbotron

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
     import Navbar from './Navbar.svelte';
+    import Jumbotron from './Jumbotron.svelte';
     import Footer from './Footer.svelte';
 </script>
 
 <!-- Navbar Component -->
 <Navbar/>
+
+<!-- Jumbotron Image -->
+<Jumbotron/>
 
 <!-- Footer Component -->
 <Footer/>
